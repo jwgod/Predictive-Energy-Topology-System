6700ae01c4d0f96520b80b400a6b66777973e996
jameslgoin <jlgoin@uw.edu>
2016-12-22 01:02:13 -0800

    map plotting, cleaned up storage, plot speed

--- Waze_Traffic.py
+++ Waze_Traffic.py
@@ -4,6 +4,10 @@
 import json
 import logging
 import requests
+import gmplot
+import webbrowser
+import matplotlib.pyplot as plt
+import numpy as np
 
 WAZE_URL = "https://www.waze.com/"
 
@@ -24,7 +28,7 @@ def address_to_coords(address):
         lat = response_json['location']['lat']
         return {"lon": lon, "lat": lat}
 
-def get_route(start_coords, end_coords):
+def get_waze_route(start_coords, end_coords):
         """Get route data from waze"""
 
         routing_req = "RoutingManager/routingRequest"
@@ -37,68 +41,121 @@ def get_route(start_coords, end_coords):
                 "returnGeometries": "true",
                 "returnInstructions": "true",
                 "timeout": 60000,
-                "nPaths": 3,
+                "nPaths": 100,
                 "options": "",
         }
+
         response = requests.get(WAZE_URL + routing_req, params=url_options)
-        print WAZE_URL, routing_req, url_options
+
         response_json = response.json()
+
         if response_json.get("error"):
                 print('ERROR')
         if response_json.get("alternatives"):
-                return response_json['alternatives'][0]['response']
+                return response_json['alternatives'][5]['response']
         return response_json['response']
 
-def calc_route_info(route):
+def hrs_to_sec(sec):
+	return (sec / (60.00*60.00))
+
+def m_to_miles(m):
+	return (m / 1000.00)*0.621371
+
+def get_route_info(r):
         """Calculate route info."""
 
-        results = route['results']
-        time = 0
-        distance = 0
+        results = r['results']
+
+        total_time = 0
+        total_distance = 0
 	node = 0 
-	r = {} 
+
+	route = {} 
+	lats = [] 
+	lons = [] 
+
         for segment in results:
 		this_time = segment['crossTime']
                 this_distance = segment['length']
-                time += this_time
-                distance += this_distance
 
-		this_time_hrs = this_time / (60.00*60.00)
-                this_distance_miles = (this_distance / 1000.00)*0.621371
+                total_time += this_time
+                total_distance += this_distance
+                 
+		this_time_hrs = hrs_to_sec(this_time)
+                this_distance_miles = m_to_miles(this_distance)
 		this_speed = this_distance_miles / this_time_hrs
+
 		path = segment['path']
-		#print  path['y'], ', ', path['x']               
-		print 'Node: ', node, ' Lat: ', path['y'], 'Lon: ', path['x'], ' at ', this_speed, 'mph for ', this_distance_miles , ' miles for ', this_time, 's' 
+           
+		#print 'Node: ', node, ' Lat: ', path['y'], 'Lon: ', path['x'], ' at ', this_speed, 'mph for ', this_distance_miles , ' miles for ', this_time, 's' 
+
 		node = node + 1
-		r[node] = (path['y'], path['x'])
+		route[node] = {} 
+		route[node]['location'] = (path['y'], path['x'])
+		lats.append(path['y'])
+		lons.append(path['x'])
+		route[node]['time'] = this_time
+		route[node]['speed'] = this_speed
+		route[node]['distance'] = this_distance_miles
+		route[node]['rel_distance'] = m_to_miles(total_distance)		
 
-        route_time = time / 60.00
-        route_distance = distance / 1000.00
-        
-        print 'Total Route Time (mins): ', route_time
-        print 'Total Route Distance (miles)', route_distance*0.621371 
+        route_distance = m_to_miles(total_distance)
 
-        return route_time, route_distance
+	route['total_time'] = total_time       
+	route['total_distance'] = route_distance
+	route['lats'] = lats
+	route['lons'] = lons
+	route['node_count'] = node
+
+        print 'Total Route Time (mins): ', total_time/60.00
+        print 'Total Route Distance (miles)', route_distance
+
+        return route
 
 def main():
 
 	from_address = 'University of Washington, Seattle, WA, United States'
 	to_address = 'Legend House, Roosevelt Way Northeast, Seattle, WA, United States'
+	url = "mymap.html"
+
 
 	start_coords = address_to_coords(from_address) 
 	end_coords = address_to_coords(to_address) 
+	gmap = gmplot.GoogleMapPlotter(start_coords["lat"], start_coords["lon"], 13)
 
 	print(start_coords)
 	print(end_coords)
 
-	route = get_route(start_coords, end_coords)
-
+	route = get_waze_route(start_coords, end_coords)
 
 	with open('raw_route_data.txt', 'w') as outfile:
 	     json.dump(route, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
 
-	calc_route_info(route)
+	route_info = get_route_info(route)
+
+	lats = route_info['lats']
+	lons = route_info['lons']
 	
+	gmap.plot(lats, lons, 'cornflowerblue', edge_width=6)
+	gmap.draw(url)
+	
+	webbrowser.open(url,new=2)
+
+	distances = []
+	times = [] 
+	speeds = [] 
+	node_count = route_info['node_count'] 
+	for i in range(1, node_count):
+		distances.append(route_info[i]['rel_distance'])
+		speeds.append(route_info[i]['speed'])
+		times.append(route_info[i]['time'])
+
+	plt.figure() 
+	plt.subplot(1, 1, 1)
+
+	plt.plot(distances, speeds)
+
+	plt.show()
 
 main()
 
